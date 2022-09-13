8e0ec87f0e529c153f2525097922c0278a2d9c6a
securvitodev <101978765+securvitodev@users.noreply.github.com>
2022-09-13 18:36:30 +0200

    Add files via upload

---
+++ Vehicle/accumlatorC.lua
@@ -0,0 +1,18 @@
+local accumlatorSubText = '%'
+
+local vehicle = getElementsByType ('vehicle')
+
+local accumlatorState = getElementData(vehicle, 'veh.accumlatorState')
+
+local defaultAccuState = setElementData(vehicle, 'veh.accumlatorState', 100)
+
+addEventHandler('onClientRender', root, function ()
+	if engineState = false  and 
+		if lampaState = true then
+			setTimer( function()
+
+			) 
+			accumlatorState = setElementData('veh.accumlatorState', accumlatorState - 0.00002)
+
+
+end)
